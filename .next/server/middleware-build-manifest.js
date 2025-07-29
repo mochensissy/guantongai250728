ffa914d2d8c99c4ea0c61c3d51f7872040211f3a
mochensissy <mochensissy@gmail.com>
2025-07-29 10:20:47 +0800

    250729对话和框架基础解决好，但尚未解决小篇幅多章节问题

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -28,6 +28,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/learn/[sessionId].js"
+    ],
+    "/upload": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/upload.js"
     ]
   },
   "ampFirstPages": []
